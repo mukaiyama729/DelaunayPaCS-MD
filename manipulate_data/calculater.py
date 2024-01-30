d00941b270ac3f8c037e8b0e481bae5d5bb6fdda
mukaiyama729 <mukamukatennis7777@icloud.com>
2024-01-30 21:37:39 +0900

    fix

--- manipulate_data/calculater.py
+++ manipulate_data/calculater.py
@@ -12,7 +12,6 @@ class Calculater:
         return rmsd
 
     def superimpose_coordinates(self, coord1, coord2):
-
         center1 = np.mean(coord1, axis=0)
         center2 = np.mean(coord2, axis=0)
         coord1_centered = coord1 - center1
@@ -20,10 +19,9 @@ class Calculater:
 
         covariance_matrix = np.dot(coord1_centered.T, coord2_centered)
         u, s, v = np.linalg.svd(covariance_matrix)
-        rotation_matrix = np.dot(u, v)
+        rotation_matrix = np.dot(v, u)
         if np.linalg.det(rotation_matrix) < 0:
-            v[:, -1] *= -1
-            rotation_matrix = np.dot(u, v)
+            rotation_matrix[:, -1] *= -1
         translation_vector = center1 - np.dot(rotation_matrix, center2.T).T
 
         coord2_transformed = np.dot(rotation_matrix, coord2.T).T + translation_vector
